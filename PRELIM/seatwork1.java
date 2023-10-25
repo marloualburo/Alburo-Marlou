4a2145c98153fffc12379cc8829c70dfe7345611
marloualburo <93631455+marloualburo@users.noreply.github.com>
2023-10-25 02:34:06 +0100

    Add files via upload

---
+++ PRELIM/seatwork1.java
@@ -0,0 +1,22 @@
+class seatwork1 {
+
+ public static void main (String [] args) {
+   System.out.println("Basic Arithmetic Program: ");
+      
+   int x = 1;
+   int y = 2;
+   int sum = 0, product = 0, difference = 0, quotient = 0;
+   
+   sum = x + y;
+   product = x * y;
+   difference = x - y;
+   quotient = x / y;
+   
+   System.out.println("Sum is " +sum);
+   System.out.println("Product is " +product);
+   System.out.println("Difference is " +difference);
+   System.out.println("Quotient is " +quotient);
+    
+   
+   }
+ }
